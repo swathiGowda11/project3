3d02d80bc074b5226186f50eb4be90ef09877699
swathiGowda11 <151748189+swathiGowda11@users.noreply.github.com>
2023-11-22 19:13:34 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,27 @@
+var roll_btn=document.getElementById("roll_btn")
+roll_btn.addEventListener("click",function(e){
+    var randomno1=Math.floor(Math.random()*6)+1
+    //console.log("Images/dice"+randomno1+".png")
+    var randomimg1="Images/dice"+randomno1+".png"
+    var img1=document.getElementById("img1")
+    img1.setAttribute("src",randomimg1)
+
+    var randomno2=Math.floor(Math.random()*6)+1
+    //console.log("Images/dice"+randomno2+".png")
+    var randomimg2="Images/dice"+randomno2+".png"
+    var img2=document.getElementById("img2")
+    img2.setAttribute("src",randomimg2)
+
+    var res=document.getElementById("res")
+    if(randomno1>randomno2){
+        res.innerHTML="Player1 Won"
+    }
+    else if(randomno2>randomno1){
+        res.innerHTML="Player2 Won"
+    }
+    else
+        res.innerHTML="Draw"
+    
+})
+
+
